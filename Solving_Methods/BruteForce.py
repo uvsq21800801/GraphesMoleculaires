ee1523fe7c76d0268c882d62c4777b5a49d06f80
uvsq21800801 <fugu.swag@gmail.com>
2022-02-16 01:22:47 +0100

    test isomorphisme sans coloriage

--- Solving_Methods/BruteForce.py
+++ Solving_Methods/BruteForce.py
@@ -1,4 +1,6 @@
 import math
+from pynauty import *
+
 
 # Explication de l'algorithme
 # On initialise la matrice de combinaison à la 1ere combinaison (aucun sommet)
@@ -14,6 +16,7 @@ import math
 
 # méthode à appeler pour bruteforcer
 def BruteF(listindex, matriceadja, atom_caract, resultat):
+    ###### Initialisations ######
     # initialisation de la matrice des combinaisons
     combi = []
     matrx_len = int(len(matriceadja))
@@ -34,7 +37,29 @@ def BruteF(listindex, matriceadja, atom_caract, resultat):
     for k in range(max_ordre - min_ordre +1):
         resultat.append([]) 
 
-    # bruteforcage
+    # Structure de stockage pour vérifier l'isomorphisme
+    # avec le certificat
+    # Dictionnaire: isomorph_List(certificat, liste_combi)
+    #liste_combi = {'0000','0001','0010'}
+    #certificat = '1'
+    isomorph_dict = {} #= {certificat: liste_combi}
+    #print(isomorph_dic)
+    #print(isomorph_dic['1'])
+
+    # initialisation des couleurs
+    couleur_nb = 0
+    dict_couleur = {}    
+    for i in atom_caract:
+        splitted = i.split()
+        if splitted[0] not in dict_couleur:
+            dict_couleur[splitted[0]] = couleur_nb 
+            couleur_nb += 1
+        #print(dict_couleur)
+    #print(couleur_nb)
+
+    #return 0;
+
+    ###### Algorithme Bruteforce ######
     for i in range(int(math.pow(2,int(matrx_len)))):
         # verification que la combinaison est connexe
         conx = verif_conx(combi, matriceadja, ordre)
@@ -43,6 +68,13 @@ def BruteF(listindex, matriceadja, atom_caract, resultat):
             affiche_combi(combi, ordre)
             
             # --> test isomorphisme
+            certif = combi_to_certif(combi, matriceadja, atom_caract, dict_couleur)
+            if certif not in isomorph_dict:
+                isomorph_dict[certif] = [combi]
+            else:
+                isomorph_dict[certif].append(combi)
+            #isomorph_list[certif] = combi
+            
             # --> calcul couverture et nb occurrance
             # --> stockage couverture en tableau de nombre d'apparition par sommet
             # --> stockage nombre d'occurance du sous-graphe (isomorphe) par un compteur
@@ -55,6 +87,64 @@ def BruteF(listindex, matriceadja, atom_caract, resultat):
         ordre = add_magique(combi, ordre)
         if ordre > max_ordre:
             break
+    
+    print (isomorph_dict)
+
+
+# Fait et retourne un certificat de graphe à partir d'une 
+# combinaison de sommets
+def combi_to_certif(combi, matriceadja, atom_caract, dict_c):
+    ###### Initialisations ######
+    
+    # création d'une liste permettant de renommer les sommets
+    # en fonction des sommets non-concernés
+    nv_nom = []
+    enum = 0
+    for i in range(len(combi)):
+        if combi[i] == 1:
+            nv_nom.append(enum)
+            enum += 1
+        else:
+            nv_nom.append('')
+
+    # initialisation de la liste des sommets classés par couleur
+    list_by_colors = []
+    for i in range(len(dict_c)):
+        list_by_colors.append([])
+
+    # initialisation du graph
+    g = Graph(enum)
+    
+    ###### Algo ######
+    # remplissage des vertex
+    sommet_n = 0
+    for i in range(len(combi)):
+        listconnex = []
+        if combi[i] == 1:
+            for j in range(len(combi)):
+                if matriceadja[i][j] != 0 and combi[j] == 1:
+                    #print(nv_nom[j])
+                    listconnex.append(nv_nom[j])
+            #g.connect_vertex(sommet_n, listconnex)
+            temp = atom_caract[i].split()
+            #dict_c.get(temp)
+            list_by_colors[dict_c.get(temp[0])].append(nv_nom[i]) #to int needed maybe
+            sommet_n += 1
+            g.connect_vertex(nv_nom[i], listconnex)
+
+    ################################## pas de coloriage pour l'instant
+    # coloriage des sommets
+    #for i in range(len(dict_c)):
+        #g.set_vertex_coloring([set(list_by_colors[i])])
+        
+        #test #g.set_vertex_coloring([set([1])])
+        #g.set_vertex_coloring([set([2])])        
+        
+    print(g)
+    print(autgrp(g))
+    #print(certificate(g))
+    
+    return certificate(g) 
 
 # methode de verification de la connexite d'une combinaison 
 # de sommets
