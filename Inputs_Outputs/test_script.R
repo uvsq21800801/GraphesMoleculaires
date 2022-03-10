ae5d664aac9372f220899de5049320acfa79ebb6
Coralie Zens <coralie.zens@ens.uvsq.fr>
2022-03-10 20:45:44 +0100

    Correction fautes d'orthographe et modif

--- Inputs_Outputs/test_script.R
+++ Inputs_Outputs/test_script.R
@@ -1,4 +1,4 @@
 mydata <- read.table("img58_6.txt", header = TRUE, sep = " ", dec = ".")
 
-plot(mydata[[1]], mydata[[3]], main="Taux de recouvrement par nombre d'occurance croissant pour des sous-graphe d'ordre 6 - img58", xlab="Groupes de sous-graphes à isomorphisme près", ylab="Nombre d'occurance", pch=20, col="blue")
+plot(mydata[[1]], mydata[[3]], main="Taux de recouvrement et nombre d'occurrence pour des motifs de 6 sommets - img58", xlab="Motifs à isomorphisme près", ylab="Nombre d'occurrence", pch=20, col="blue")
 text(mydata[[1]], mydata[[3]], labels=mydata[[2]], cex=labels.size, pos=labels.position)
