86f3810433540be1fd60411835232be0e4458a9a
uvsq21800801 <fugu.swag@gmail.com>
2022-04-12 23:11:03 +0200

    MCIS (att ya des chances que ça marche pas parfaitement)

--- Main.py
+++ Main.py
@@ -1,13 +1,33 @@
 # Python[version]
 # Main, File to execute
 import sys, os
-GraphesMoleculaires = os.getcwd()
-path = os.path.dirname(GraphesMoleculaires)
-sys.path.append('GraphesMoleculaires/UserInterface')
-from User_interface import Command_interface
+from os import listdir, remove
+from os.path import isfile, join
+import re
+sys.path.append('MCIS/analysis')
 
-def main():
-    Command_interface.interface()
+from analysis import mcis
     
+def main():
+
+    SiO_caract = ['Si 1', 'O 2']
+
+    SiO_adja = [[0,1],[1,0]]
+
+    SiO4_caract = ['Si 1', 'O 2', 'O 3', 'O 4', 'O 5']
+
+    SiO4_adja = [[0,1,0,1,1],
+                [1,0,1,0,1],
+                [1,1,0,1,1],
+                [0,0,1,0,1],
+                [1,1,0,1,0]]
+    
+    mcis.mcis_algo(SiO4_adja,SiO4_caract,SiO_adja,SiO_caract)
+
+
 if __name__=="__main__":
     main()
+
+
+
+
---
+++ analysis/mcis.py
@@ -0,0 +1,53 @@
+from os import listdir, remove
+from os.path import isfile, join
+import re
+import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def mcis_algo(a_adja, a_car, b_adja, b_car):
+    # declaration de graph a et graph b
+    g_a = nx.Graph()
+    g_b = nx.Graph()
+
+    print(a_car)
+    print(a_adja)
+
+    
+    a_len = int(len(a_adja))
+    for i in range(a_len):
+        splitted = a_car[i].split()
+        color = splitted[0]
+        g_a.add_nodes_from([(i, {"color": color})])
+        for j in range(a_len):
+            if(a_adja[i][j] == 1):
+                g_a.add_edges_from([(i, j)] , color="blue" )
+            if(a_adja[i][j] == 2):
+                g_a.add_edges_from([(i, j)] , color="red" )
+
+    b_len = int(len(b_adja))
+    for i in range(b_len):
+        splitted = b_car[i].split()
+        color = splitted[0]
+        g_b.add_nodes_from([(i, {"color": color})])
+        for j in range(b_len):
+            if(b_adja[i][j] == 1):
+                g_b.add_edges_from([(i, j)] , color="blue" )
+            if(b_adja[i][j] == 2):
+                g_b.add_edges_from([(i, j)] , color="red")
+
+    
+    # MCIS
+    ismags = nx.isomorphism.ISMAGS(g_b,g_a)
+    largest_common_subgraph = list(ismags.largest_common_subgraph())
+
+
+
+
+    #affichages
+    print(g_a)
+
+    print(g_b)
+
+    print(largest_common_subgraph)
